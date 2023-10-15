130e1c49b27e0340708ded6e253c8ef71193483c
Anastasiia Morozova <nmorozova283@gmail.com>
2023-10-15 19:30:14 +0300

    hw 2.1.3 added task_score.js

---
+++ task_score.js
@@ -0,0 +1,12 @@
+let introScore = 100;
+let gitScore = 180;
+let jsScore = 240;
+
+let avgScore = Math.floor((introScore + gitScore + jsScore)/3);
+
+//console.log(avgScore);
+
+let studentName = "Eren";
+let studentSurname = "Yeager";
+
+console.log(`${studentName} ${studentSurname}\`s average score for the Intro, Git and JS modules is ${avgScore}.`);
